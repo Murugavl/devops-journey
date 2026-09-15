1ebeff7f88c82e87723a93311d69829be04dfa9b
Murugavl <vmv2k05@gmail.com>
2026-09-15 06:12:18 +0000

    add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "enter a folder name to create : "
+read folder_name
+
+
+if [ -d "$folder_name" ]
+then
+    echo "This already exists"
+else
+    echo "Creating it now"
+    for i in logs data config cache tmp
+    do
+        mkdir -p "$folder_name/$i"
+    done
+    echo "folder created successfully"
+fi
